00719765f39daf52361a7d8fa455e6706b3a8a29
mloginov <mikhail.loginov@citronium.com>
2019-02-19 17:08:31 +0300

    Override Tab, Backspace, Delete in soft tabs mode

--- addon/navigation/navigateWithinSoftTabs.js
+++ addon/navigation/navigateWithinSoftTabs.js
@@ -11,6 +11,79 @@
   var LEFT_KEY = 37;
   var RIGHT_KEY = 39;
 
+  var emptyRegex = /^\s*$/;
+
+  function repeatStringNumTimes(string, times) {
+    if (times > 0)
+      return string.repeat(times);
+    else
+      return "";
+  }
+
+  function insertSmartTab(cm) {
+    if (cm.getOption('indentWithTabs')) {
+      cm.replaceSelection("\t", "end", "+input");
+    } else {
+      var cursor = cm.getCursor();
+      var tabSize = cm.getOption('tabSize');
+      var numSpaces = (Math.floor(cursor.ch / tabSize) + 1) * tabSize - cursor.ch;
+      cm.replaceSelection(repeatStringNumTimes(" ", numSpaces), "end", "+input");
+    }
+  }
+
+  function smartTab(cm) {
+    cm.operation(function (){
+      if (cm.somethingSelected()) {
+        cm.indentSelection("add");
+      } else {
+        insertSmartTab(cm);
+      }
+    });
+  }
+
+  function smartDelete(cm) {
+    cm.operation(function (){
+      var sel = cm.doc.sel, doc = cm.doc;
+      if (sel.somethingSelected()) {
+        doc.replaceSelection("", null, "+delete");
+      } else {
+        var cursor = cm.getCursor();
+        var tabSize = cm.getOption('tabSize');
+        var end = cm.findPosH(cursor, tabSize, "column");
+        if (end && end.line === cursor.line) {
+          var word = cm.getRange(cursor, end);
+          if ((cursor.ch % tabSize) === 0 && emptyRegex.test(word)) {
+            cm.doc.replaceRange("", cursor, end);
+          } else {
+            cm.deleteH(1, "char");
+          }
+        } else {
+          cm.deleteH(1, "char");
+        }
+      }
+    });
+  }
+
+  function smartBackspace(cm) {
+    cm.operation(function (){
+      var sel = cm.doc.sel, doc = cm.doc;
+      if (sel.somethingSelected()) {
+        doc.replaceSelection("", null, "+delete");
+      } else {
+        var cursor = cm.getCursor();
+        var tabSize = cm.getOption('tabSize');
+        var start = {line: cursor.line, ch: cursor.ch - tabSize};
+        var word = cm.getRange(start, cursor);
+        if (cursor.ch >= tabSize && (cursor.ch % tabSize) === 0 && emptyRegex.test(word)) {
+          cm.doc.replaceRange("", start, cursor);
+        } else {
+          cm.deleteH(-1, "char");
+        }
+      }
+    });
+  }
+
+  var prevExtraKeys = null;
   CodeMirror.defineOption("navigateWithinSoftTabs", null, function (cm, val, old) {
     var prev = old == CodeMirror.Init ? null : old;
     if (val == prev) return
@@ -20,6 +93,7 @@
       cm.off("mousedown", onMouseDown);
       cm.off('cursorActivity', cursorActivity);
       cm.state.navigateWithinSoftTabs = {};
+      cm.setOption("extraKeys", prevExtraKeys);
     }
     if (val) {
       // turn on navigation
@@ -31,6 +105,12 @@
         startPos: null,
         direction: null
       };
+      prevExtraKeys = cm.getOption("extraKeys");
+      cm.setOption("extraKeys", {
+        Tab: smartTab,
+        Delete: smartDelete,
+        Backspace: smartBackspace
+      });
     }
   });
 
